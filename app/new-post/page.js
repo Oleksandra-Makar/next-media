a00e72d8127533b2634eff0dd9b661d396d9204a
Oleksandra <oleksandra.201153@gmail.com>
2025-11-11 20:54:41 +0200

    create post implemented

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,11 +1,26 @@
+import FormSubmit from "@/components/form-submit";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
 export default function NewPostPage() {
+  async function createPost(formData) {
+    "use server";
+
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
+
+    await storePost({ title, imageUrl: "", content, userId: 1 });
+
+    redirect("/feed");
+  }
   return (
     <>
       <h1>Create a new post</h1>
-      <form>
+      <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title" required />
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
@@ -14,15 +29,15 @@ export default function NewPostPage() {
             accept="image/png, image/jpeg"
             id="image"
             name="image"
+            required
           />
         </p>
         <p className="form-control">
           <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
+          <textarea id="content" name="content" rows="5" required />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
