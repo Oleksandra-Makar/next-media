4a22e36eb1a152d8a729c1022111e2771be6bd54
Oleksandra <oleksandra.201153@gmail.com>
2025-11-13 10:10:59 +0200

    like/unlike logic implemented

--- components/form-submit.js
+++ components/form-submit.js
@@ -1,13 +1,14 @@
 "use client";
 
-import { useFormStatus } from "react-dom";
+import { useFormStatus } from 'react-dom';
 
 export default function FormSubmit() {
   const status = useFormStatus();
 
   if (status.pending) {
-    return <p>Submitting...</p>;
+    return <p>Creating post...</p>;
   }
+
   return (
     <>
       <button type="reset">Reset</button>
